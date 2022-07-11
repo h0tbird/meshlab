d412d1e71d9f99182cc7cdbda9edd0cb0d1d1fbd
Marc Villacorta <h0tbird@github.com>
2022-07-11 18:12:51 +0200

    Fix networking

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # it-works-on-my-machine
 Local k8s on arm64
+
+```
+multipass get local.driver
+qemu
+```
+
+```
+qemu-system-aarch64 \
+  -machine virt,highmem=off \
+  -accel hvf \
+  -drive file=/Library/Application Support/com.canonical.multipass/bin/../Resources/qemu/edk2-aarch64-code.fd,if=pflash,format=raw,readonly=on \
+  -cpu cortex-a72 \
+  -nic vmnet-shared,model=virtio-net-pci,mac=52:54:00:6a:db:ad \
+  -device virtio-scsi-pci,id=scsi0 \
+  -drive file=/var/root/Library/Application Support/multipassd/qemu/vault/instances/kube-02/ubuntu-20.04-server-cloudimg-arm64.img,if=none,format=qcow2,discard=unmap,id=hda \
+  -device scsi-hd,drive=hda,bus=scsi0.0 \
+  -smp 2 \
+  -m 2048M \
+  -qmp stdio \
+  -chardev null,id=char0 \
+  -serial chardev:char0 \
+  -nographic \
+  -cdrom /var/root/Library/Application Support/multipassd/qemu/vault/instances/kube-02/cloud-init-config.iso
+```
+
+```
+calicoctl get ippool -o wide
+
+```
--- lib/misc.sh
+++ lib/misc.sh
@@ -18,13 +18,16 @@ function launch_k8s {
 	- path: /var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl
 	  content: CltwbHVnaW5zLm9wdF0KICBwYXRoID0gIi92YXIvbGliL3JhbmNoZXIvazNzL2FnZW50L2NvbnRhaW5lcmQiCgpbcGx1Z2lucy5jcmldCiAgc3RyZWFtX3NlcnZlcl9hZGRyZXNzID0gIjEyNy4wLjAuMSIKICBzdHJlYW1fc2VydmVyX3BvcnQgPSAiMTAwMTAiCiAgZW5hYmxlX3NlbGludXggPSBmYWxzZQogIHNhbmRib3hfaW1hZ2UgPSAicmFuY2hlci9taXJyb3JlZC1wYXVzZTozLjYiCgpbcGx1Z2lucy5jcmkuY29udGFpbmVyZF0KICBzbmFwc2hvdHRlciA9ICJvdmVybGF5ZnMiCiAgZGlzYWJsZV9zbmFwc2hvdF9hbm5vdGF0aW9ucyA9IHRydWUKCltwbHVnaW5zLmNyaS5jb250YWluZXJkLnJ1bnRpbWVzLnJ1bmNdCiAgcnVudGltZV90eXBlID0gImlvLmNvbnRhaW5lcmQucnVuYy52MiIKCltwbHVnaW5zLmNyaS5jb250YWluZXJkLnJ1bnRpbWVzLnJ1bmMub3B0aW9uc10KCVN5c3RlbWRDZ3JvdXAgPSBmYWxzZQoKW3BsdWdpbnMuY3JpLnJlZ2lzdHJ5Lm1pcnJvcnNdCiAgW3BsdWdpbnMuY3JpLnJlZ2lzdHJ5Lm1pcnJvcnMuImRvY2tlci5pbyJdCiAgICBlbmRwb2ludCA9IFsiaHR0cDovLzE5Mi4xNjguNjQuMTo1MDAxIl0K
 	  encoding: b64
+	- path: /etc/calico-installation.yaml
+	  content: LS0tCmFwaVZlcnNpb246IG9wZXJhdG9yLnRpZ2VyYS5pby92MQpraW5kOiBJbnN0YWxsYXRpb24KbWV0YWRhdGE6CiAgbmFtZTogZGVmYXVsdApzcGVjOgogIGNhbGljb05ldHdvcms6CiAgICBjb250YWluZXJJUEZvcndhcmRpbmc6IEVuYWJsZWQKICAgIGlwUG9vbHM6CiAgICAtIGJsb2NrU2l6ZTogMjYKICAgICAgY2lkcjogMTAuNDIuMC4wLzE2CiAgICAgIGVuY2Fwc3VsYXRpb246IFZYTEFOQ3Jvc3NTdWJuZXQKICAgICAgbmF0T3V0Z29pbmc6IEVuYWJsZWQKICAgICAgbm9kZVNlbGVjdG9yOiBhbGwoKQotLS0KYXBpVmVyc2lvbjogb3BlcmF0b3IudGlnZXJhLmlvL3YxCmtpbmQ6IEFQSVNlcnZlciAKbWV0YWRhdGE6IAogIG5hbWU6IGRlZmF1bHQgCnNwZWM6IHt9Cg==
+	  encoding: b64
 	runcmd:
 	- 'curl -sfL https://get.k3s.io |
 	  INSTALL_K3S_EXEC="--flannel-backend=none --cluster-cidr=10.42.0.0/16 --disable-network-policy --disable=traefik" \
 	  CONTAINERD_LOG_LEVEL="debug" \
 	  K3S_KUBECONFIG_MODE="644" sh -s -'
 	- kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
-	- kubectl create -f https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml
+	- kubectl create -f /etc/calico-installation.yaml
 	- sleep 10; kubectl wait --for=condition=Ready nodes --all --timeout=60s
 	- kubectl label --overwrite node $1 topology.kubernetes.io/region=$1
 	- IP=\$(hostname -I | awk '{print \$1}')

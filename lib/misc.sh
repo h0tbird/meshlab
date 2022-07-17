dec422783d2ae007cd08d4fcefa332a0740ed920
Marc Villacorta <h0tbird@github.com>
2022-07-17 10:46:14 +0200

    Install the step CLI tool

--- lib/misc.sh
+++ lib/misc.sh
@@ -1,9 +1,8 @@
 #------------------------------------------------------------------------------
-# Base64 encode config files
+#
 #------------------------------------------------------------------------------
 
-CONTAINERD_CONFIG=$(base64 -w0 conf/containerd.tmpl)
-CALICO_CONFIG=$(base64 -w0 conf/calico.yaml)
+CA_DIR=$(mktemp --tmpdir="${TMPDIR:-/tmp}" -d root-ca.XXXXX)
 
 #------------------------------------------------------------------------------
 #
@@ -16,6 +15,12 @@ CALICO_CONFIG=$(base64 -w0 conf/calico.yaml)
 
 function launch_k8s {
 
+  # Base64 encoded config files
+  CONTAINERD_CONFIG=$(base64 -w0 conf/containerd.tmpl)
+  CALICO_CONFIG=$(base64 -w0 conf/calico.yaml)
+  ROOTCA_CERT=$(base64 -w0 ${CA_DIR}/root-cert.pem)
+  ROOTCA_KEY=$(base64 -w0 ${CA_DIR}/root-key.pem)
+
   # Setup the VM with cloud-config
   multipass launch --name $1 --cpus 2 --mem 2G --disk 8G --mount tmp/$1:/mnt/host --cloud-init - <<- EOF
 	#cloud-config
@@ -26,18 +31,53 @@ function launch_k8s {
 	- path: /etc/calico-installation.yaml
 	  content: ${CALICO_CONFIG}
 	  encoding: b64
+	- path: /etc/root-cert.pem
+	  content: ${ROOTCA_CERT}
+	  encoding: b64
+	- path: /etc/root-key.pem
+	  content: ${ROOTCA_KEY}
+	  encoding: b64
 	runcmd:
 	- |
+	  
+	  #-------------
+	  # Install k3s
+	  #-------------
+	  
 	  curl -sfL https://get.k3s.io |
 	  INSTALL_K3S_EXEC="--flannel-backend=none --cluster-cidr=10.42.0.0/16 --disable-network-policy --disable=traefik" \
 	  CONTAINERD_LOG_LEVEL="debug" \
 	  K3S_KUBECONFIG_MODE="644" sh -s -
+	  
+	  #----------------
+	  # Install Calico
+	  #----------------
+	  
 	  kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
 	  kubectl create -f /etc/calico-installation.yaml
 	  sleep 10; kubectl wait --for=condition=Ready nodes --all --timeout=60s
 	  kubectl label --overwrite node $1 topology.kubernetes.io/region=$1
+	  
+	  #------------
+	  # Kubeconfig
+	  #------------
+	  
 	  IP=\$(hostname -I | awk '{print \$1}')
 	  kubectl config view --raw | sed "s/127\.0\.0\.1/\${IP}/g; s/: default/: $1/g" > /home/ubuntu/config
+	  
+	  #--------------------
+	  # Generate Istio ICA
+	  #--------------------
+	  
+	  [ "\$(hostname)" != "kube-00" ] && {
+	    wget -qO- https://github.com/smallstep/cli/releases/download/v0.21.0/step_linux_0.21.0_$(arch).tar.gz |
+	    tar zxv --strip-components=2 -C /usr/bin/ step_0.21.0/bin/step
+	  }
+	  
+	  #----------------
+	  # Install ArgoCD
+	  #----------------
+	  
 	  [ "\$(hostname)" = "kube-00" ] && {
 	     kubectl create ns argocd
 	     kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml

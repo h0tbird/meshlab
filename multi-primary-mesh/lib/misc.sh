9b539ffb9edd8cb3a7f9c93cfb1334172dc738b8
Marc Villacorta <h0tbird@github.com>
2023-03-21 12:26:32 +0100

    Cleanup

--- multi-primary-mesh/lib/misc.sh
+++ multi-primary-mesh/lib/misc.sh
@@ -54,8 +54,6 @@ function launch_k8s {
   # Base64 encoded config files
   REG_CONFIG=$(base64 -w0 conf/registries.yaml)
   K3S_CONFIG=$(base64 -w0 conf/k3s.yaml)
-  # ROOTCA_CERT=$(base64 -w0 ./tmp/istio-ca/root-cert.pem)
-  # ROOTCA_KEY=$(base64 -w0 ./tmp/istio-ca/root-key.pem)
 
   # Setup the VM with cloud-config
   multipass launch --name "$1" --cpus 2 --memory 5G --disk 10G --mount "tmp/$1:/mnt/host" --cloud-init - <<- EOF
@@ -68,14 +66,6 @@ function launch_k8s {
 	- path: /etc/rancher/k3s/config.yaml
 	  content: ${K3S_CONFIG}
 	  encoding: b64
-	# - path: /etc/certs/root-cert.pem
-	#   content: ${ROOTCA_CERT}
-	#   permissions: '0600'
-	#   encoding: b64
-	# - path: /etc/certs/root-key.pem
-	#   content: ${ROOTCA_KEY}
-	#   permissions: '0600'
-	#   encoding: b64
 	 
 	runcmd:
 	- |
@@ -103,45 +93,6 @@ function launch_k8s {
 	  
 	  IP=\$(hostname -I | awk '{print \$1}')
 	  kubectl config view --raw | sed "s/127\.0\.0\.1/\${IP}/g; s/: default/: $1/g" > /home/ubuntu/config
-	  
-	  # #--------------------
-	  # # Generate Istio ICA
-	  # #--------------------
-	  # 
-	  # # Install step CLI
-	  # wget -qO- https://github.com/smallstep/cli/releases/download/v0.21.0/step_linux_0.21.0_$(arch).tar.gz |
-	  # tar zxv --strip-components=2 -C /usr/bin/ step_0.21.0/bin/step
-	  # 
-	  # [ "\$(hostname)" != "kube-00" ] && {
-	  # 
-	  #   # Generate ICA
-	  #   step certificate create \
-	  #   "Istio intermediate CA" \
-	  #   /etc/certs/ca-cert.pem \
-	  #   /etc/certs/ca-key.pem \
-	  #   --ca /etc/certs/root-cert.pem \
-	  #   --ca-key /etc/certs/root-key.pem \
-	  #   --profile intermediate-ca \
-	  #   --san *.example.com \
-	  #   --not-after 43800h \
-	  #   --no-password \
-	  #   --insecure \
-	  #   --kty RSA
-	  # 
-	  #   # Generate bundle
-	  #   step certificate bundle \
-	  #   /etc/certs/ca-cert.pem \
-	  #   /etc/certs/root-cert.pem \
-	  #   /etc/certs/cert-chain.pem
-	  # 
-	  #   # Create a cacerts secret
-	  #   kubectl create namespace istio-system
-	  #   kubectl -n istio-system create secret generic cacerts \
-	  #   --from-file=/etc/certs/ca-cert.pem \
-	  #   --from-file=/etc/certs/ca-key.pem \
-	  #   --from-file=/etc/certs/root-cert.pem \
-	  #   --from-file=/etc/certs/cert-chain.pem
-	  # }
 	  
 	  #----------------
 	  # Install ArgoCD

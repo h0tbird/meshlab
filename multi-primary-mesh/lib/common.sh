b17ff0c0445639c8f97aebf4135e39f77146baff
Marc Villacorta <h0tbird@github.com>
2023-03-30 19:45:50 +0200

    Remove unused variable

--- multi-primary-mesh/lib/common.sh
+++ multi-primary-mesh/lib/common.sh
@@ -5,12 +5,6 @@ export CLUS0="kube-00"
 export CLUS1="kube-01"
 export CLUS2="kube-02"
 
-# Define a stamp
-export -A STAMP=(
-  [name]="red-balloon"
-  [clusters]="${CLUS1} ${CLUS2}"
-)
-
 export -A STAMPS=(
   [red-balloon]="${CLUS1} ${CLUS2}"
   #[pizza]="pizza-1 pizza-2"

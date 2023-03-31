ca7faa9b8b64bb2513803cbad3881fa82b9c4f06
Marc Villacorta <h0tbird@github.com>
2023-03-31 11:29:55 +0200

    Update ./bin/delete

--- multi-primary-mesh/lib/common.sh
+++ multi-primary-mesh/lib/common.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# Cluster names
+#------------------------------------------------------------------------------
+# Define stamps and clusters
+#------------------------------------------------------------------------------
+
 export CLUS0="kube-00"
 export CLUS1="pasta-1"
 export CLUS2="pasta-2"
@@ -9,3 +12,21 @@ export -A STAMPS=(
   [pasta]="${CLUS1} ${CLUS2}"
   [pizza]="pizza-1 pizza-2"
 )
+
+#------------------------------------------------------------------------------
+# List clusters and stamps
+#------------------------------------------------------------------------------
+
+function list {
+  case $1 in
+    "clusters")
+      [ "${2:-stamp}" == "all" ] && echo -n "${CLUS0} "
+      for STAMP in "${!STAMPS[@]}"; do
+        echo -n "${STAMPS[${STAMP}]} "
+      done ;;
+    "stamps")
+      for STAMP in "${!STAMPS[@]}"; do
+        echo -n "${STAMP} "
+      done ;;
+  esac
+}

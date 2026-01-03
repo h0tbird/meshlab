dc5a9bd4b4d7288c57982f2f7ef0bd4c51020319
Marc Villacorta <h0tbird@github.com>
2026-01-03 14:15:59 +0000

    Set DOCKER_API_VERSION

--- lib/common.sh
+++ lib/common.sh
@@ -18,7 +18,11 @@ declare -A CELLS=(
 PWD='meshlab123'
 
 # Runnable sections (in order)
-SECTIONS=()
+export SECTIONS=()
+
+# Auto-detect Docker API version
+DOCKER_API_VERSION=$(curl -s --unix-socket /var/run/docker.sock http://localhost/version | jq -r '.ApiVersion')
+export DOCKER_API_VERSION
 
 #------------------------------------------------------------------------------
 # List cells and clusters

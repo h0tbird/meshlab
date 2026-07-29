9349de6954104c199147a7848482210cf4d70973
h0tbird <h0tbird@users.noreply.github.com>
2026-07-29 16:50:43 +0000

    deploy: acd476e3fc9420d245c10576a80698408f724499

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-12c89d97.js',
+                path_to_root + 'searchindex-025bbdab.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
